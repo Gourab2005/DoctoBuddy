5798248be1e5ceb5671bfc3bab93b9534812070e
Gourab Das <dgourab574@gmail.com>
2024-05-17 02:21:42 +0530

    smooth scroll

--- script.js
+++ script.js
@@ -18,6 +18,24 @@ let tween = gsap.to(".title", {
     startAt: {y: -300, opacity: 0},
     ease:"bounce.out"
 });
+
+// smoothscroll used from : https://github.com/darkroomengineering/lenis
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+// end
+
 gsap.to(".entry1", { 
     startAt:{y:-200,opacity:0},
     y: 0,
@@ -30,5 +48,27 @@ gsap.to(".entry2", {
     y: 0,
     x: 0,
     duration: 2,
+    opacity:1
+});
+
+gsap.to(".headingentry-section2",{
+    scrollTrigger: ".headingentry-section2",
+    startAt:{x:-200,opacity:0},
+    x:0,
+    duration:1,
+    opacity:1
+});
+gsap.to(".headingentry-section3",{
+    scrollTrigger: ".headingentry-section3",
+    startAt:{x:-200,opacity:0},
+    x:0,
+    duration:1,
+    opacity:1
+});
+gsap.to(".headingentry-section4",{
+    scrollTrigger: ".headingentry-section4",
+    startAt:{x:-200,opacity:0},
+    x:0,
+    duration:1,
     opacity:1
 });
